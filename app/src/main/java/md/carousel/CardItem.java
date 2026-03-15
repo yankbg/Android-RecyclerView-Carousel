ad5abb0ba37f16c2d590ff73a0aeee1ea71cc49e
YANNICK kabunga <yankbg2@gmail.com>
2026-03-15 23:21:46 +0300

    we create data model to represent each card’s content.

---
+++ app/src/main/java/md/carousel/CardItem.java
@@ -0,0 +1,25 @@
+package md.carousel;
+
+public class CardItem {
+    private int imageResId;
+    private String title;
+    private String description;
+
+    public CardItem(int imageResId, String title, String description) {
+        this.imageResId = imageResId;
+        this.title = title;
+        this.description = description;
+    }
+
+    public int getImageResId() {
+        return imageResId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
